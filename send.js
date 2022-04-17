339c89d21965ea00487c3d2e7aa1c2c758235b79
Denis <denelvis@yandex.ru>
2022-04-18 00:03:51 +0300

    simple queue publisher and consumer

--- send.js
+++ send.js
@@ -7,6 +7,12 @@ const sendMsg = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
   await channel.assertQueue(queueName, { durable: false });
+  channel.sendToQueue(queueName, Buffer.from(msg));
+  console.log("Sent: ", msg);
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
 };
 
 sendMsg();
