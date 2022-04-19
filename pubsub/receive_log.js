b7eca808026389e8452abadd71d10181b4959e65
Denis <denelvis@yandex.ru>
2022-04-19 19:38:42 +0300

    added receive log fanout

---
+++ pubsub/receive_log.js
@@ -0,0 +1,21 @@
+const amqplib = require("amqplib");
+
+const exchangeName = "logs";
+
+const receiveLog = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  await channel.assertExchange(exchangeName, "fanout", { durable: false });
+  const q = await channel.assertQueue("", { exclusive: true });
+  console.log(`Waiting for messages in queue: ${q.queue}`);
+  channel.bindQueue(q.queue, exchangeName, "");
+  channel.consume(
+    q.queue,
+    (msg) => {
+      if (msg.content) console.log("The message is: ", msg.content.toString());
+    },
+    { noAck: true }
+  );
+};
+
+receiveLog();
