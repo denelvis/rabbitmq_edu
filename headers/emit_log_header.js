96f88f864e2a6291fbdc7e5c761f96fedcc883ac
Denis Shkabenkov <denis.shkabenkov@bsc-ideas.com>
2022-04-20 12:25:47 +0300

    added headers exchange, added emit file

---
+++ headers/emit_log_header.js
@@ -0,0 +1,21 @@
+const amqplib = require("amqplib");
+
+const exchangeName = "header_logs";
+const args = process.argv.slice(2);
+const msg = args[0] || "Hello world";
+
+const emitHeader = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  await channel.assertExchange(exchangeName, "headers", { durable: false });
+  channel.publish(exchangeName, "", Buffer.from(msg), {
+    headers: { account: "new", method: "google" },
+  });
+  console.log("Sent: ", msg);
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+};
+
+emitHeader();
