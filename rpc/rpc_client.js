2e05b1a9cddd04a03b89b3f28b1180ae141e56ac
Denis Shkabenkov <denis.shkabenkov@bsc-ideas.com>
2022-04-21 16:48:30 +0300

    added rpc_client, uuid lib

---
+++ rpc/rpc_client.js
@@ -0,0 +1,32 @@
+const amqplib = require("amqplib");
+const { v4: uuidv4 } = require("uuid");
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("Usage: rpc_client.js num");
+  process.exit(1);
+}
+
+const num = parseInt(args[0]);
+const uuid = uuidv4();
+
+const getFib = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  const q = await channel.assertQueue("", { exclusive: true });
+
+  console.log(" [x] Requesting fib(%d)", num);
+
+  channel.sendToQueue("rpc_queue", Buffer.from(num.toString()), {
+    replyTo: q.queue,
+    correlationId: uuid,
+  });
+  console.log("Sent: ", num);
+  setTimeout(() => {
+    connection.close();
+    process.exit(0);
+  }, 500);
+};
+
+getFib();
