65561aa50320feb5aa29f73031f04a4bb4638224
Denis Shkabenkov <denis.shkabenkov@bsc-ideas.com>
2022-04-22 10:10:29 +0300

    rpc server

---
+++ rpc/rpc_server.js
@@ -0,0 +1,37 @@
+const amqplib = require("amqplib");
+
+const queueName = "rpc_queue";
+
+const seq = {};
+function fibonacci(num) {
+  if (num === 0 || num === 1) return num;
+  if (num in seq) return seq[num];
+  return (seq[num] = fibonacci(n - 1) + fibonacci(n - 2));
+}
+
+const sendFib = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queueName, { durable: false });
+  channel.prefetch(1);
+  console.log("[x] Awaiting RPC requests");
+
+  channel.consume(
+    queueName,
+    (msg) => {
+      const n = parseInt(msg.content.toString());
+      console.log("[.] fib(%d)", n);
+
+      const fibNum = fibonacci(n);
+      channel.sendToQueue(
+        msg.properties.replyTo,
+        Buffer.from(fibNum.toString()),
+        { correlationId: msg.properties.correlationId }
+      );
+      channel.ack(msg);
+    },
+    { noAck: false }
+  );
+};
+
+sendFib();
